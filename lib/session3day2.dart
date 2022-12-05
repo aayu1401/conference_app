a94410336bd6bcecf04b2f71005ee05635ef99a8
Aayushi Mittal <65257348+aayu1401@users.noreply.github.com>
2022-12-05 18:24:01 +0530

    Update session3day2.dart

--- lib/session3day2.dart
+++ lib/session3day2.dart
@@ -44,7 +44,7 @@ class Session3day2 extends StatelessWidget {
                         fontStyle: FontStyle.normal,
                         fontFamily: 'Poppins',
                         fontSize: 12),),
-                  const Text("14 December 2022 (Wednesday)",
+                  const Text("15 December 2022 (Thursday)",
                     style: const TextStyle(
                         fontWeight: FontWeight.w600,
                         fontStyle: FontStyle.normal,
